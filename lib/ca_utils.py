daf2c8bf6f12417496e05e2d011e4510db335a4c
Charles Butler <chuck@dasroot.net>
2016-07-27 12:10:00 -0500

    Generation of CA Certificates
    This bit of logic implements the methods and logic required to 'bootstrap' the Certificate Authority by generating the CA keys.

--- lib/ca_utils.py
+++ lib/ca_utils.py
@@ -1,7 +1,27 @@
-from subprocess import check_call
+from charmhelpers.core.hookenv import log
 from shlex import split
+
+from subprocess import check_call
+from subprocess import check_output
+from subprocess import CalledProcessError
 
 
 def install(src, tgt):
-    ''' This method wraps the bash "install" command '''
+    """ This method wraps the bash "install" command """
     return check_call(split('install {} {}'.format(src, tgt)))
+
+
+def get_output(cmd):
+    """ Execute a command on the host. Supports bare string
+    input.
+
+    :param cmd: string of the command to Execute
+    :returns: The output of the executed command
+    """
+    try:
+        log('Executing: {}'.format(cmd), 'debug')
+        out = check_output(split(cmd))
+        return out.decode('ascii')
+    except CalledProcessError as e:
+        log('Error executing command. reason: {}'.format(e.stdout), 'critical')
+        return e.stdout
--- reactive/certificate_authority.py
+++ reactive/certificate_authority.py
@@ -1,21 +1,30 @@
 from charms.reactive import when
 from charms.reactive import when_not
 from charms.reactive import set_state
+from charms.templating.jinja2 import render
+from charmhelpers.core.hookenv import config
 from charmhelpers.core.hookenv import resource_get
 from charmhelpers.core.hookenv import status_set
+from charmhelpers.core.unitdata import kv
+
 from ca_utils import install
 
+from tempfile import NamedTemporaryFile
+
 import cfssl
+import cfssljson
 
 
 @when_not('certificate-authority.installed')
-def install_certificate_authority():
+def install_cfssl():
+    """ Install the CFSSL binaries """
+    status_set('maintenance', 'Installing CFSSL.')
     # Resources are used to deliver the CA binary bits
     cfssl_bin = resource_get('cfssl')
     cfssl_json_bin = resource_get('cfssljson')
 
     if not cfssl_bin or not cfssl_json_bin:
-        status_set('blocked', 'Missing resources. See: README.')
+        status_set('blocked', 'Missing resources. See: README.md.')
         return
 
     install(cfssl_bin, '/usr/local/bin/cfssl')
@@ -27,4 +36,16 @@ def install_certificate_authority():
 @when('certificate-authority.installed')
 @when_not('certificate-authority.initialized')
 def initialize_ca():
-    cfssl.sign()
+    """ Initialize the certificate authority with keys """
+    render('csr_ca.json', '/etc/cfssl/csr_ca.json', config())
+    response = cfssl.gencert('/etc/cfssl/csr_ca.json', initca=True)
+    temp = NamedTemporaryFile(suffix='.json')
+
+    # write out the response to a temporary file, and generate the certificates
+    # from the response json.
+    with open(temp.name, 'w') as fp:
+        fp.write(response)
+        # when we are in context, we need to flush the buffer to actually write
+        fp.flush()
+        # Generate TLS keys from the JSON response
+        cfssljson.parse("ca", bare=True, f=temp.name)
